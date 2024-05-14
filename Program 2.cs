3a5d7a3112d3f42188bd208c73abd03656cd4551
Максим <123723682+Maxim634@users.noreply.github.com>
2024-05-14 21:24:00 +0300

    Add files via upload

---
+++ Program 2.cs
@@ -0,0 +1,54 @@
+﻿enum Weekdays
+{
+    Понедельник = 1,
+    Вторник,
+    Среда,
+    Четверг,
+    Пятница,
+    Суббота,
+    Воскресенье
+}
+enum DayTime
+{
+    Утро,
+    День,
+    Вечер,
+    Ночь
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите день недели (1-7): ");
+        int day = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите время (0-23): ");
+        int time = Convert.ToInt32(Console.ReadLine());
+
+        Weekdays inputDay = (Weekdays)day;
+        DayTime timeOfDay = GetTimeOfDay(time);
+
+        Console.WriteLine($"Сейчас {inputDay}, {timeOfDay}");
+    }
+
+    static DayTime GetTimeOfDay(int time)
+    {
+        if (time >= 7 && time <= 12)
+        {
+            return DayTime.Утро;
+        }
+        else if (time >= 13 && time <= 17)
+        {
+            return DayTime.День;
+        }
+        else if (time >= 18 && time <= 23)
+        {
+            return DayTime.Вечер;
+        }
+        else
+        {
+            return DayTime.Ночь;
+        }
+    }
+}
